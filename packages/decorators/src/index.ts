2db116c0d50f9287d050e17aa21f5e5372b5462d
anchan828 <anchan828@gmail.com>
2019-10-08 14:17:36 +0900

    fix: export EncryptColumn

--- packages/decorators/src/index.ts
+++ packages/decorators/src/index.ts
@@ -1,4 +1,5 @@
 export { BooleanColumn } from "./boolean-column";
+export { EncryptColumn } from "./encrypt-column";
 export { HmacColumn } from "./hmac-column";
 export { StaticFileColumn } from "./static-file-column";
 export { UlidColumn } from "./ulid-column";
