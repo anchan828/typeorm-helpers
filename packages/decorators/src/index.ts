03e11ad303db3069079c17b6b07c9aa89f90edf6
anchan828 <anchan828@gmail.com>
2020-08-12 18:18:17 +0900

    fix(decorators): export JsonColumn

--- packages/decorators/src/index.ts
+++ packages/decorators/src/index.ts
@@ -1,5 +1,6 @@
 export { BooleanColumn } from "./boolean-column";
 export { EncryptColumn } from "./encrypt-column";
 export { HmacColumn } from "./hmac-column";
+export { JsonColumn } from "./json-column";
 export { StaticFileColumn } from "./static-file-column";
 export { UlidColumn } from "./ulid-column";
