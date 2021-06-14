f6f909658a4cbfb4886d81b913b8d64ce49dfe17
Keigo Ando <anchan828@gmail.com>
2021-06-14 23:37:33 +0900

    chore: add meny-to-many test

--- packages/history/src/history-entity.spec-e2e.ts
+++ packages/history/src/history-entity.spec-e2e.ts
@@ -5,6 +5,9 @@ import {
   Entity,
   EventSubscriber,
   getConnection,
+  getRepository,
+  JoinTable,
+  ManyToMany,
   PrimaryGeneratedColumn,
 } from "typeorm";
 import { ulid } from "ulid";
@@ -152,4 +155,170 @@ describe("e2e test (basic)", () => {
   });
 
   afterEach(() => getConnection().close());
+});
+
+describe("e2e test (many-to-many)", () => {
+  @Entity()
+  class Category {
+    @PrimaryGeneratedColumn()
+    public id!: number;
+
+    @Column()
+    public name!: string;
+  }
+
+  @Entity()
+  class Question {
+    @PrimaryGeneratedColumn()
+    public id!: number;
+
+    @Column()
+    public title!: string;
+
+    @Column()
+    public text!: string;
+
+    @ManyToMany(() => Category, { cascade: true })
+    @JoinTable()
+    public categories!: Category[];
+  }
+
+  @Entity()
+  class CategoryHistory extends Category implements HistoryEntityInterface {
+    @Column()
+    public originalID!: number;
+
+    @HistoryActionColumn()
+    public action!: HistoryActionType;
+
+    @PrimaryGeneratedColumn()
+    public id!: number;
+  }
+
+  @Entity()
+  class QuestionHistory extends Question implements HistoryEntityInterface {
+    @Column()
+    public originalID!: number;
+
+    @HistoryActionColumn()
+    public action!: HistoryActionType;
+
+    @PrimaryGeneratedColumn()
+    public id!: number;
+  }
+
+  @EventSubscriber()
+  class CategoryHistorySubscriber extends HistoryEntitySubscriber<Category, CategoryHistory> {
+    public entity = Category;
+    public historyEntity = CategoryHistory;
+  }
+
+  @EventSubscriber()
+  class QuestionHistorySubscriber extends HistoryEntitySubscriber<Question, QuestionHistory> {
+    public entity = Question;
+    public historyEntity = QuestionHistory;
+  }
+
+  beforeEach(async () => {
+    const connection = await createConnection({
+      database: "test",
+      dropSchema: true,
+      entities: [Category, CategoryHistory, Question, QuestionHistory],
+      host: process.env.DB_HOST || "localhost",
+      password: "root",
+      subscribers: [CategoryHistorySubscriber, QuestionHistorySubscriber],
+      synchronize: true,
+      type: (process.env.DB_TYPE || "mysql") as any,
+      username: "root",
+    });
+    expect(connection).toBeDefined();
+    expect(connection.isConnected).toBeTruthy();
+  });
+
+  afterEach(() => getConnection().close());
+
+  it("should same", async () => {
+    // create tests
+    const category1 = new Category();
+    category1.name = "animals";
+
+    const category2 = new Category();
+    category2.name = "zoo";
+
+    const question = new Question();
+    question.title = "dogs";
+    question.text = "who let the dogs out?";
+    question.categories = [category1, category2];
+    await getConnection().manager.save(question);
+
+    await expect(getRepository(Question).find({})).resolves.toEqual([
+      {
+        id: 1,
+        text: "who let the dogs out?",
+        title: "dogs",
+      },
+    ]);
+
+    await expect(getRepository(QuestionHistory).find({})).resolves.toEqual([
+      {
+        action: "CREATED",
+        id: 1,
+        originalID: 1,
+        text: "who let the dogs out?",
+        title: "dogs",
+      },
+    ]);
+
+    await expect(getRepository(Category).find({})).resolves.toEqual([
+      { id: 1, name: "animals" },
+      { id: 2, name: "zoo" },
+    ]);
+
+    await expect(getRepository(CategoryHistory).find({})).resolves.toEqual([
+      { action: "CREATED", id: 1, name: "animals", originalID: 1 },
+      { action: "CREATED", id: 2, name: "zoo", originalID: 2 },
+    ]);
+
+    // update tests
+    question.text = "updated";
+    question.categories[0].name = "updated";
+
+    await getConnection().manager.save(question);
+
+    await expect(getRepository(Question).find({})).resolves.toEqual([
+      {
+        id: 1,
+        text: "updated",
+        title: "dogs",
+      },
+    ]);
+
+    await expect(getRepository(QuestionHistory).find({})).resolves.toEqual([
+      {
+        action: "CREATED",
+        id: 1,
+        originalID: 1,
+        text: "who let the dogs out?",
+        title: "dogs",
+      },
+      {
+        action: "UPDATED",
+        id: 2,
+        originalID: 1,
+        text: "updated",
+        title: "dogs",
+      },
+    ]);
+
+    await expect(getRepository(Category).find({})).resolves.toEqual([
+      { id: 1, name: "updated" },
+      { id: 2, name: "zoo" },
+    ]);
+
+    await expect(getRepository(CategoryHistory).find({})).resolves.toEqual([
+      { action: "CREATED", id: 1, name: "animals", originalID: 1 },
+      { action: "CREATED", id: 2, name: "zoo", originalID: 2 },
+      { action: "UPDATED", id: 3, name: "updated", originalID: 1 },
+    ]);
+  });
 });
