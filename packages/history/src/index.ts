3b53fe94debdcf94b2a33d84e202c727be537cc1
Keigo Ando <anchan828@gmail.com>
2021-07-14 03:35:53 +0900

    fix: export dropUniqueIndices

--- packages/history/src/index.ts
+++ packages/history/src/index.ts
@@ -1,3 +1,4 @@
 export { HistoryActionType } from "./history-action.enum";
 export { HistoryActionColumn, HistoryEntityInterface, HistoryOriginalIdColumn } from "./history-entity";
+export { dropUniqueIndices } from "./history-migration";
 export { HistoryEntitySubscriber } from "./history-subscriber";
