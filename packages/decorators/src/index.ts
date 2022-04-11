fa466976b5d3e2ec6e5b75b948da2de6b8f968eb
Keigo Ando <anchan828@gmail.com>
2022-04-11 09:48:48 +0900

    fix: export ulid functions

--- packages/decorators/src/index.ts
+++ packages/decorators/src/index.ts
@@ -3,4 +3,4 @@ export { EncryptColumn } from "./encrypt-column";
 export { HmacColumn } from "./hmac-column";
 export { JsonColumn } from "./json-column";
 export { StaticFileColumn } from "./static-file-column";
-export { UlidColumn } from "./ulid-column";
+export { PrimaryUlidColumn, UlidColumn, UlidColumnOptions } from "./ulid-column";
